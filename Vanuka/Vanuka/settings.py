b51cd39d0739237f245345a6b7b9b6e8e2604bc8
Arasteh Zamen Ghadirli <41484641+ArastehZz@users.noreply.github.com>
2025-09-01 18:36:46 +0330

    models_product_created

--- Vanuka/Vanuka/settings.py
+++ Vanuka/Vanuka/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "product",
-    "accounting"
+    "accounting",
+    "phonenumber_field"
 ]
 
 MIDDLEWARE = [
--- Vanuka/product/models.py
+++ Vanuka/product/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+
+
+
+class product(models.Model):
+    name = models.CharField(max_length=20)
+    weight = models.PositiveIntegerField()
+    craftingfee_buy = models.PositiveBigIntegerField()
+    craftingfee_sell = models.PositiveBigIntegerField()
+    Is_available = models.BooleanField()
+    bonakdaricode = models.CharField(max_length=30)
+    bonakdariname = models.CharField(max_length=40)
+    
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone_number = PhoneNumberField(unique=True, region="IR")
+    
+        
+    def __str__(self):
+        return f"{self.name} - {self.phone_number}"
+    
+    
+    
